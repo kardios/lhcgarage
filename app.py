a152bae21152d2be208276cd8409b55d447228c4
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2023-12-19 23:36:58 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import os
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+
+# Retrieve the OpenAI API key from the environment variables
+API_KEY = os.environ["ANTHROPIC_API_KEY"]
+
+anthropic = Anthropic(
+    api_key = API_KEY,
+)
+
+st.write("LHC's Garage")
+st.text_input("Enter your prompt:", "How does a man become a god?")
+
+completion = anthropic.completions.create(
+    model="claude-2.1",
+    temperature = 0,
+    max_tokens_to_sample=1000,
+    prompt=f"{HUMAN_PROMPT} {input} {AI_PROMPT}",
+)
+output = completion.completion
+
+st.write(output)
