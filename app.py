feaa05501e45ae2065cb831a41bfe02c9abf4b4d
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2023-12-23 13:21:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,17 +16,17 @@ st.write("LHC's Garage")
 
 Menu_Option = st.selectbox("Select analysis:", ('Shorten the text into a summary', 'Identify possible biases in the text', 'Seek views disagreeing with the text', 'Find angles missing from the text', 'Discuss broader significance of the topics', 'Compare the text with historical events', 'Customise your own unique prompt'))
 if Menu_Option == "Shorten the text into a summary":
-  instruction = "You are my helpful reading assistant. Please read the text below and generate a concise and coherent summary that includes the main ideas and key details."
+  instruction = "Generate a concise and coherent summary that includes the main ideas and key details."
 elif Menu_Option == "Identify possible biases in the text":
-  instruction = "You are my helpful reading assistant. Please read the text below and highlight any possible biases."
+  instruction = "Identify possible biases in the text."
 elif Menu_Option == "Seek views disagreeing with the text":
-  instruction = "You are my helpful reading assistant. Please read the text below and offer perspectives that disagree with the text."
+  instruction = "Offer perspectives that disagree with the text."
 elif Menu_Option == "Find angles missing from the text":
-  instruction = "You are my helpful reading assistant. Please read the text below and offer perspectives that are missing from the text."
+  instruction = "Offer perspectives that are missing from the text."
 elif Menu_Option == "Discuss broader significance of the topics":
-  instruction = "You are my helpful reading assistant. Please read the text below and draft a conclusion that highlights the broader significance of the topics."
+  instruction = "Draft a conclusion that highlights the broader significance of the topics."
 elif Menu_Option == "Compare the text with historical events":
-  instruction = "You are my helpful reading assistant. Please read the text below and reflect on the text and draw similiarities and differences to historical events in the last century."
+  instruction = "Reflect on the text and draw similiarities and differences to historical events in the last century."
 elif Menu_Option == "Customise your own unique prompt":
   instruction = st.text_input("Customise your own unique prompt:", "What are the follow up actions?")
 
@@ -42,7 +42,7 @@ if uploaded_file is not None:
 
 if st.button('Let\'s Go!'):
     start = time.time()
-    input = instruction + "\n\n" + raw_text
+    input = "Read the text below." + instruction + "\n\n" + raw_text
     completion = anthropic.completions.create(
         model="claude-2.1",
         temperature = 0,
