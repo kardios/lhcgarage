9d5f09caf6e85e06ba2267754835d44509baa897
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-03-29 09:39:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,17 +19,19 @@ st.write("LHC's Garage :sunglasses: Testing OpenAI, Anthropic and Google's LLMs 
 
 Model_Option = st.selectbox("**Select** model:", ('gpt-4-turbo-preview','claude-3-opus-20240229','gemini-1.5-pro'))
 
-Option_Action = st.selectbox("What should I do with your input?", ('Condense into key points', 'Shorten into a summary', 'Identify possible biases', 'Identify disagreeing views', 'Identify missing angles', 'Discuss broader significance', 'Compare with historical events', 'Black swans and grey rhinos', 'Generate markdown summary', 'Customise your own prompt'))
-if Option_Action == "Condense into key points":
-  instruction = "You are my reading assistant. You will read the input I provide. Summarize the input into bullet points. Identify the main ideas and key details, and condense them into concise bullet points. Recognize the overall structure of the text and create bullet points that reflect this structure. The output should be presented in a clear and organized way. Do not start with any titles."
-elif Option_Action == "Shorten into a summary":
+Option_Action = st.selectbox("What should I do with your input?", ('Shorten into a summary', 'Condense into key points', 'Identify possible biases', 'Identify disagreeing views', 'Identify missing angles', 'Create alternative mental models', 'Discuss broader significance', 'Compare with historical events', 'Black swans and grey rhinos', 'Generate markdown summary', 'Customise your own prompt'))
+if Option_Action == "Shorten into a summary":
   instruction = "You are my reading assistant. You will read the input I provide. Generate a concise and coherent summary. Identify the main ideas and key details. Present your output in a clear and organised way, as one single paragraph only."
+elif Option_Action == "Condense into key points":
+  instruction = "You are my reading assistant. You will read the input I provide. Summarize the input into bullet points. Identify the main ideas and key details, and condense them into concise bullet points. Recognize the overall structure of the text and create bullet points that reflect this structure. The output should be presented in a clear and organized way. Do not start with any titles."
 elif Option_Action == "Identify possible biases":
   instruction = "You are my reading assistant. You will read the input I provide. Highlight any possible biases in the input in a clear and organised way, as one or more paragraphs."
 elif Option_Action == "Identify disagreeing views":
   instruction = "You are my reading assistant. You will read the input I provide. Offer perspectives that disagree with the input in a clear and organised way, as one or more paragraphs."
 elif Option_Action == "Identify missing angles":
   instruction = "You are my reading assistant. You will read the input I provide. Offer perspectives that are missing from the input in a clear and organised way, as one or more paragraphs."
+elif Option_Action == "Create alternative mental models":
+  instruction = "You are my reading assistant. You will read the input I provide. Generate three alternative mental models to consider the topics in the input in a clear and organised way."
 elif Option_Action == "Discuss broader significance":
   instruction = "You are my reading assistant. You will read the input I provide. Draft a conclusion that highlights the broader significance of the topics in the input. Present the output in a clear and organised way, as one or more paragraphs."
 elif Option_Action == "Compare with historical events":
