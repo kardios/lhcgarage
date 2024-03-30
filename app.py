c930f034dd5f755d8b1c312a3a7004d09204f8f1
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-03-30 15:51:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ if raw_text.strip() != "":
     st.write("Time to generate: " + str(round(end-start,2)) + " seconds")
     st.download_button(':scroll:', output_text)
   except:
-    st.error(" Error occurred during data analysis.", icon="🚨")
+    st.error(" Error occurred when running the model.", icon="🚨")
