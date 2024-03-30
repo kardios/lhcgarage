29f94fafaa66aeb6ee5c8bef1cf4dfbeb5dd5beb
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-03-30 15:51:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ if raw_text.strip() != "":
     st.write("Time to generate: " + str(round(end-start,2)) + " seconds")
     st.download_button(':scroll:', output_text)
   except:
-    st.error("  An error occurred when trying to analyse the data.", icon="🚨")
+    st.error(" Error occurred during data analysis.", icon="🚨")
