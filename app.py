2ca690967f380c14917da10df4c9655b07d5003c
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-04-27 11:44:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,9 @@ if st.button("Let\'s Go! :rocket:"):
   
   with st.spinner("Running AI Model..."):
     start = time.time()
-    prompt = "You are the most amazing intern in the world. Today is " + date.today() + ".\n\n" + Customised_Prompt + "\n\n" + input_text
+    prompt = "You are the most amazing intern in the world. Today is " + date.today() + ".\n\n"
+    st.write(prompt)
+    prompt = prompt + Customised_Prompt + "\n\n" + input_text
     gemini = genai.GenerativeModel("gemini-1.5-pro-latest")
     response = gemini.generate_content(prompt, safety_settings = safety_settings, generation_config = generation_config)
     answer = response.text
