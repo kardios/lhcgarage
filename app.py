af6666504f009f2c1286217c3211f48c2ef50bff
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-04-28 21:42:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ generation_config = genai.GenerationConfig(
   temperature = 0,
 )
 
-st.set_page_config(page_title="Sherwood Labz", page_icon=":sunglasses:",)
+st.set_page_config(page_title="Sherwood Lab", page_icon=":sunglasses:",)
 
 with open("documentation.txt") as doc_file:
     doc_text = doc_file.read()
